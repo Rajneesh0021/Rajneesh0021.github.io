fb2811e8c8e8669670b842db6983e8e9157146c8
rajneesh <mr.rajneeshy@gmail.com>
2023-03-30 01:09:55 +0530

    done

--- portfolio.js
+++
@@ -1,9 +0,0 @@
-const menuicon =document.querySelector('#menu-icon')
-
-const navbar =document.querySelector('.navbar')
-const navbg=document.querySelector('.nav-bg')
-menuicon.addEventListener('click',()=>{
-    menuicon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
-    navbg.classList.toggle('active')
-})
